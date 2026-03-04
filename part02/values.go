ecddc1f113e728d75ca881427e5c4059f2eb903c
Perro2110 <perrottamarco2011@gmail.com>
2026-03-04 11:00:24 +0100

    comment file

--- part02/values.go
+++ part02/values.go
@@ -5,11 +5,15 @@ package main
 import "fmt"
 
 func main() {
+	// String values are surrounded by double quotes
 	fmt.Println("go" + "lnag")
 
+	// Integer and floating point numbers are added, subtracted, multiplied, and divided as expected
 	fmt.Println("1 + 1 = ", 1+1)
+	// Integer division discards the fractional part
 	fmt.Println("7.0/3.0 = ", 7.0/3.0)
 
+	// Boolean values are used with boolean operators as expected
 	fmt.Println(true && false)
 	fmt.Println(true || false)
 	fmt.Println(!true)
--- part03/variables.go
+++ part03/variables.go
@@ -5,18 +5,22 @@ package main
 import "fmt"
 
 func main() {
+	// Variables can be declared with an initial value
 	var a = "init"
 	fmt.Println(a)
 
+	// Multiple variables can be declared at once
 	var b, c int = 1, 2
 	fmt.Println(b, c)
 
+	// Variables declared without an initial value are zero-valued
 	var d = true
 	fmt.Println(d)
 
 	var e int
 	fmt.Println(e)
 
+	// The := syntax is shorthand for declaring and initializing a variable in one line
 	f := "pizza"
 	fmt.Println(f)
 
