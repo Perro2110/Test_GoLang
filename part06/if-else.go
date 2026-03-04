b7edc1103adbc5925f3527ee421633da8387f264
Perro2110 <perrottamarco2011@gmail.com>
2026-03-04 11:10:05 +0100

    if else !

---
+++ part06/if-else.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	if 7%2 == 0 {
+		fmt.Println("7 is even")
+	} else {
+		fmt.Println("7 is odd")
+	}
+
+	if 8%4 == 0 {
+		fmt.Println("8 is divisible by 4")
+	}
+
+	if 8%2 == 0 || 7%2 == 0 {
+		fmt.Println("8 or 7 is divisible by 2")
+	}
+
+	// A statement can precede conditionals;
+	// any variables declared in this statement are availabl
+	// in the current and all subsequent branches.
+	a := 90
+	if num := a; num < 0 {
+		fmt.Println(num, "is negative")
+	} else if num < 10 {
+		fmt.Println(num, "has 1 digit")
+	} else {
+		fmt.Println(num, "has multiple digits")
+	}
+
+}
